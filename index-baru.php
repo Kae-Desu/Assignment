7024bafb4e97ddf28822cafb24de140fd3d7e522
楓 <87841341+Kae-Desu@users.noreply.github.com>
2023-05-02 11:35:13 +0700

    git{P{USH
    yagitu

--- index-baru.php
+++ index-baru.php
@@ -41,9 +41,12 @@
                         <td><?= $row['position'] ?></td>
                     </tr>
                     <?php
+                    $count = $count + 1;
                 }
+                echo "<h1>patch redlock 2.1</h1>"
+                echo "<p>changelog: add count</p>"
+                echo "<h1>Total: {$count}</h1>";
             ?>
         <bodyt>
     </table>
 </body>
-
